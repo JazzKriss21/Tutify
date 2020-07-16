b1433aa4e0eb34db0af1822c5f64ff5295a7b986
darshangovindaraj <55881834+darshangovindaraj@users.noreply.github.com>
2020-07-16 22:16:25 +0530

    added result (incomplete)

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,5 +6,5 @@ app_name = 'dashboard'
 urlpatterns = [
     path('student/', views.student_dashboard_view, name='student_dashboard'),
     path('tutor/', views.tutor_dashboard_view, name='tutor_dashboard'),
-
-]
+    path('resultsearch/', views.result_search_view, name='result_search')
+] 
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,3 +7,6 @@ def student_dashboard_view(request):
 
 def tutor_dashboard_view(request):
 	return render(request, 'dashboard/tutorpage_afterlogin.html')
+
+def result_search_view(request):
+	return render(request, 'dashboard/student_page_result.html')
